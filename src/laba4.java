a0c720de362465e267f847d1a9b9a4eeac5db6ee
Blooming100 <93271792+Blooming100@users.noreply.github.com>
2021-10-27 18:15:56 +0300

    Update laba4.java

--- src/laba4.java
+++ src/laba4.java
@@ -1,9 +1,10 @@
 public class laba4 {
     public static void main(String[] args) {
 
-        System.out.println("Second commit");
+    
         System.out.println("IDE changes");
         System.out.println("Commit of GitHub.");
+        System.out.println("Commit Danil.");
 
     }
 }
